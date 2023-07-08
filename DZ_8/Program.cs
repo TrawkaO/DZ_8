0c0c841b7a2d68b3bb8fc46a346d63715ea95864
qscok <qscok@DESKTOP-K101NP5>
2023-07-08 11:02:47 +0300

    Добавьте файлы проекта.

---
+++ DZ_8/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DZ_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя Пёселя: ");
+            string dogName = Console.ReadLine();
+            Dog dog = new Dog();
+            dog.SetName(dogName);
+            Console.WriteLine($"Кличка Пёселя - {dog.GetName()}");
+            dog.Eat();
+        }
+
+        abstract class Animal
+        {
+            public string Name { get; set; }
+
+            public void SetName(string name)
+            {
+                Name = name;
+            }
+
+            public string GetName()
+            {
+                return Name;
+
+            }
+
+            public abstract void Eat();
+
+        }
+        class Dog : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine($"{Name} Хрум Хрум.");
+
+            }
+
+
+        }
+
+    }
+
+}
